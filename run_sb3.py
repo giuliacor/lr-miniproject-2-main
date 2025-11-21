51652d3a6b95a2b17f6c219b1645769dcd7b4fb4
jiriblahaa <jiri.blaha@robotwin.cz>
2025-11-21 18:59:44 +0100

    gc1

--- run_sb3.py
+++ run_sb3.py
@@ -26,7 +26,7 @@ DEBUG_MODE = True
 LEARNING_ALG = "PPO" # or "SAC"
 LOAD_NN = False # if you want to initialize training with a previous model 
 NUM_ENVS = 1    # how many pybullet environments to create for data collection
-USE_GPU = False # make sure to install all necessary drivers 
+USE_GPU = True # make sure to install all necessary drivers 
 
 # after implementing, you will want to test how well the agent learns with your MDP: 
 # env_configs = {"motor_control_mode":"CPG",
